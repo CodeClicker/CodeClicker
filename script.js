14c342810b76d38b34254afd16c1a771bb449233
CodeClicker <50332906+CodeClicker@users.noreply.github.com>
2019-05-06 10:58:28 -0500

    Right Click
    No nonsense with inspect element

--- script.js
+++ script.js
@@ -47,6 +47,30 @@ document.onkeyup = function(e) {
     document.getElementById("linesUp").disabled = false;
     } 
 }
+
+//disables right click
+document.addEventListener('keydown', function() {
+    if (event.keyCode == 123) {
+      return false;
+    } else if (event.ctrlKey && event.shiftKey && event.keyCode == 73) {
+      return false;
+    } else if (event.ctrlKey && event.keyCode == 85) {
+      return false;
+    }
+  }, false);
+  
+  if (document.addEventListener) {
+    document.addEventListener('contextmenu', function(e) {
+      e.preventDefault();
+    }, false);
+  } else {
+    document.attachEvent('oncontextmenu', function() {
+      window.event.returnValue = false;
+    });
+  }
+
+
+
 //buys autoclickers
 
 function internsUp() {
